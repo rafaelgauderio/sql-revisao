3cc630ac04aaabae41c784b12d72288dcce7553d
Rafael De Luca <deluca1712@gmail.com>
2021-02-09 17:38:00 -0300

    exercicio aula 4

--- aula_04_exercicio01.sql
+++ aula_04_exercicio01.sql
@@ -1,15 +1,19 @@
-CREATE TABLE Funcionario 
-( cod_func NUMBER(10) CONSTRAINT pk_cod_func PRIMARY KEY,
-  nome VARCHAR2(50),
-  idade NUMBER(4),
-  salario NUMBER(10,2),
-  setor VARCHAR2(30)
-);
-
 CREATE TABLE MINHA_LOG
 (DT_LOG DATE,
 DS_LOG VARCHAR2(4000)
 );
 
+DECLARE 
+    TYPE log_info_reg IS RECORD
+    ( dt minha_log.dt_log%TYPE,
+      texto_log minha_log.ds_log%TYPE      
+    );
+    registro_log log_info_reg;
+    BEGIN    
+    registro minha_log.dt := sysdate;
+    registro minha_log.texto_log := 'Eu' || user || 'serei um excelente desenvolvedor Pl/SQL.';
+    INSERT INTO minha_log VALUES registro_log;
+    END;
+    
 
 
